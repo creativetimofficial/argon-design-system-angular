9489fec4054ecf394bdcac771676015143dd1680
chelaruc <chelaruciprian10@gmail.com>
2019-07-09 15:05:27 +0300

    added copyrights

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,20 @@
+/*!
+
+=========================================================
+* Argon Design System Angular - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-design-system-angular
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-angular/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
@@ -10,4 +27,3 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
-
