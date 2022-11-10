9660488ed17a3247ac4b31bbb777ad2c4d7e887e
KasaSandhya <svanapalli@baylogictech.com>
2022-11-10 15:30:00 +0530

    Added search by specialities section

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
 
 @Component({
   selector: "app-landing",
   templateUrl: "./landing.component.html",
   styleUrls: ["./landing.component.scss"],
 })
+
 export class LandingComponent implements OnInit {
+  @ViewChild('widgetsContent') widgetsContent: ElementRef;
   focus: any;
   focus1: any;
 
   constructor() {}
 
   ngOnInit() {}
+
+  scrollLeft(){
+    this.widgetsContent.nativeElement.scrollLeft -= 150;
+  }
+
+  scrollRight(){
+    this.widgetsContent.nativeElement.scrollLeft += 150;
+  }
 }
