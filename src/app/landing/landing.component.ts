7fdca8de4728ac00fd373eb62b15e751957f4334
madhulikagadang <gadangmadhulika@gmail.com>
2022-11-10 17:08:00 +0530

    sympptoms committed

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -5,9 +5,10 @@ import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
   templateUrl: "./landing.component.html",
   styleUrls: ["./landing.component.scss"],
 })
-
 export class LandingComponent implements OnInit {
-  @ViewChild('widgetsContent') widgetsContent: ElementRef;
+  @ViewChild("widgetsContent") widgetsContent: ElementRef;
+  @ViewChild("widgetsSymptoms") widgetsSymptoms: ElementRef;
+
   focus: any;
   focus1: any;
 
@@ -15,11 +16,18 @@ export class LandingComponent implements OnInit {
 
   ngOnInit() {}
 
-  scrollLeft(){
+  scrollLeft() {
     this.widgetsContent.nativeElement.scrollLeft -= 220;
   }
 
-  scrollRight(){
+  scrollRight() {
     this.widgetsContent.nativeElement.scrollLeft += 220;
+  }
+  scrollLeftSymp() {
+    this.widgetsSymptoms.nativeElement.scrollLeft -= 220;
+  }
+
+  scrollRightSymp() {
+    this.widgetsSymptoms.nativeElement.scrollLeft += 220;
   }
 }
