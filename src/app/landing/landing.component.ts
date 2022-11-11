4db6bd8360ee111c9172d01978a757affa04cd27
madhulikagadang <gadangmadhulika@gmail.com>
2022-11-11 12:48:41 +0530

    minor fixes

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -11,22 +11,47 @@ export class LandingComponent implements OnInit {
   @ViewChild("widgetsDiagnosis") widgetsDiagnosis: ElementRef;
 
   specialitiesImgs = [
-    { img: './assets/img/hospital/physician.svg', title: 'Physician' },
-    { img: './assets/img/hospital/gynaecologist.svg', title: 'Gynaecologist'},
-    { img: './assets/img/hospital/pediatrician.svg', title: 'Pediatrician'},
-    { img: './assets/img/hospital/orthopedician.svg', title: 'Orthopedician'},
-    { img: './assets/img/hospital/eye-specialist.svg', title: 'Eye Specialist'},
-    { img: './assets/img/hospital/physiotherapist.svg', title: 'physiotherapist'},
-    { img: './assets/img/hospital/dentist.svg', title: 'Dentist'},
-    { img: './assets/img/hospital/more.svg', title: 'View all'},
+    { img: "./assets/img/hospital/physician.svg", title: "Physician" },
+    { img: "./assets/img/hospital/gynaecologist.svg", title: "Gynaecologist" },
+    { img: "./assets/img/hospital/pediatrician.svg", title: "Pediatrician" },
+    { img: "./assets/img/hospital/orthopedician.svg", title: "Orthopedician" },
+    {
+      img: "./assets/img/hospital/eye-specialist.svg",
+      title: "Eye Specialist",
+    },
+    {
+      img: "./assets/img/hospital/physiotherapist.svg",
+      title: "physiotherapist",
+    },
+    { img: "./assets/img/hospital/dentist.svg", title: "Dentist" },
+    { img: "./assets/img/hospital/more.svg", title: "View all" },
   ];
 
   diagnosisImgs = [
-    { img: './assets/img/hospital/self-check-Migraine.png', title: 'Migraine' },
-    { img: './assets/img/hospital/selfcheck-Diabetes.png', title: 'Diabetes'},
-    { img: './assets/img/hospital/selfcheck-Thyroid.png', title: 'Thyroid'},
-    { img: './assets/img/hospital/selfcheck-Heart-Health.png', title: 'Heart Health'},
-    { img: './assets/img/hospital/selfcheck-COVID.png', title: 'COVID-19'},
+    { img: "./assets/img/hospital/self-check-Migraine.png", title: "Migraine" },
+    { img: "./assets/img/hospital/selfcheck-Diabetes.png", title: "Diabetes" },
+    { img: "./assets/img/hospital/selfcheck-Thyroid.png", title: "Thyroid" },
+    {
+      img: "./assets/img/hospital/selfcheck-Heart-Health.png",
+      title: "Heart Health",
+    },
+    { img: "./assets/img/hospital/selfcheck-COVID.png", title: "COVID-19" },
+  ];
+  symptomsImgs = [
+    { img: "./assets/img/hospital/Acidity.png", title: "Acidity" },
+    { img: "./assets/img/hospital/Acne-01.png", title: "Acne" },
+    { img: "./assets/img/hospital/Anxiety.png", title: "Anxiety" },
+    { img: "./assets/img/hospital/Back-Pain.png", title: "Back-Pain" },
+    {
+      img: "./assets/img/hospital/Constipation.png",
+      title: "Constipation",
+    },
+    {
+      img: "./assets/img/hospital/Cough-01.png",
+      title: "Cough",
+    },
+    { img: "./assets/img/hospital/Depression-01.png", title: "Depression" },
+    { img: "./assets/img/hospital/Fever-01.png", title: "Fever" },
   ];
 
   focus: any;
@@ -42,19 +67,19 @@ export class LandingComponent implements OnInit {
     this.widgetsContent.nativeElement.scrollLeft += 230;
   }
   scrollLeftSymp() {
-    this.widgetsSymptoms.nativeElement.scrollLeft -= 220;
+    this.widgetsSymptoms.nativeElement.scrollLeft -= 230;
   }
 
   scrollRightSymp() {
-    this.widgetsSymptoms.nativeElement.scrollLeft += 220;
+    this.widgetsSymptoms.nativeElement.scrollLeft += 230;
   }
 
   scrollLeftDiagno() {
-    this.widgetsDiagnosis.nativeElement.scrollLeft -= 220;
+    this.widgetsDiagnosis.nativeElement.scrollLeft -= 230;
   }
 
   scrollRightDiagno() {
-    this.widgetsDiagnosis.nativeElement.scrollLeft += 220;
+    this.widgetsDiagnosis.nativeElement.scrollLeft += 230;
   }
 
   ngOnInit() {
