7ef9f96570c2203a6b1fae58220fd65bf58061f6
padma <pkanakala@baylogictech.com>
2022-11-14 11:50:54 +0530

    Added search by symptoms page

---
+++ src/app/authentication/symptoms/symptoms.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-symptoms',
+  templateUrl: './symptoms.component.html',
+  styleUrls: ['./symptoms.component.scss']
+})
+export class SymptomsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
