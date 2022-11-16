958146e15ee8bef657af27db6ce50869273e7548
padma <pkanakala@baylogictech.com>
2022-11-16 13:32:47 +0530

    Fixed some UI issues and added continue button

--- src/app/authentication/symptoms/symptoms.component.ts
+++ src/app/authentication/symptoms/symptoms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-symptoms',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SymptomsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+  getSpecialitiesList() {
+      console.log("in the list")
+      this.router.navigateByUrl("/physicians");
   }
 
 }
